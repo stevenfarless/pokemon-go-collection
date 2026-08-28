16068d5e0b63df187ac66f17c1855f95e1f71bdc
Steven <125781365+stevenfarless@users.noreply.github.com>
2026-08-28 03:33:06 -0500

    Run Storage Cleanup and Search Builder JavaScript tests

--- scripts/run_js_unit_tests.js
+++ scripts/run_js_unit_tests.js
@@ -21,6 +21,7 @@ const tests = [
   "test_battle_labs.js",
   "test_opportunity_special_labs.js",
   "test_trade_resource_labs.js",
+  "test_storage_search_labs.js",
 ];
 
 for (const testFile of tests) require(path.resolve(__dirname, "..", "tests", testFile));
